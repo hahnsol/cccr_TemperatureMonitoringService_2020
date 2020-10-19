2f640e657f245f87c305a5bc84f91753dc20346e
soltaminv <soltaminv@gmail.com>
2020-10-19 20:07:45 +0900

    return

--- src/main/java/com/cccr/armtong/config/WebConfig.java
+++ src/main/java/com/cccr/armtong/config/WebConfig.java
@@ -3,48 +3,48 @@ package com.cccr.armtong.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.thymeleaf.spring5.SpringTemplateEngine;
-import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
-import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+// import org.thymeleaf.spring5.SpringTemplateEngine;
+// import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+// import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
 @Configuration
 public class WebConfig {
     
-    // @Bean
-    // public InternalResourceViewResolver getInternalResourceViewResolver() {
+    @Bean
+    public InternalResourceViewResolver getInternalResourceViewResolver() {
         
-    //     // 이게 원래는 static으로 처음 시작되는 데, static은 html만 집어넣을 수 있어서, /WEB-INF/jsp/ (jsp)가 처음으로 되도록 경로 변경!
-    //     // InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-    //     // resolver.setPrefix("/WEB-INF/jsp/");
-    //     // resolver.setSuffix(".jsp");
-    //     // resolver.setOrder(1000);
-    //     // return resolver;
+        //이게 원래는 static으로 처음 시작되는 데, static은 html만 집어넣을 수 있어서, /WEB-INF/jsp/ (jsp)가 처음으로 되도록 경로 변경!
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/jsp/");
+        resolver.setSuffix(".jsp");
+        resolver.setOrder(1000);
+        return resolver;
+    }
+
+
+    // @Bean
+    // public SpringResourceTemplateResolver templateResolver() {
+    //     SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+    //     templateResolver.setCacheable(false);
+    //     templateResolver.setPrefix("/WEB-INF/jsp/");
+    //     templateResolver.setSuffix(".jsp");
+    //     return templateResolver;
     // }
 
+    // @Bean
+    // public SpringTemplateEngine templateEngine() {
+    //     SpringTemplateEngine springTemplateEngine = new SpringTemplateEngine();
+    //     springTemplateEngine.addTemplateResolver(templateResolver());
+    //     return springTemplateEngine;
+    // }
 
-    @Bean
-    public SpringResourceTemplateResolver templateResolver() {
-        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
-        templateResolver.setCacheable(false);
-        templateResolver.setPrefix("/WEB-INF/jsp/");
-        templateResolver.setSuffix(".jsp");
-        return templateResolver;
-    }
-
-    @Bean
-    public SpringTemplateEngine templateEngine() {
-        SpringTemplateEngine springTemplateEngine = new SpringTemplateEngine();
-        springTemplateEngine.addTemplateResolver(templateResolver());
-        return springTemplateEngine;
-    }
-
-    @Bean
-    public ThymeleafViewResolver viewResolver() {
-        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
-        viewResolver.setTemplateEngine(templateEngine());
-        viewResolver.setOrder(1);
-        return viewResolver;
-    }
+    // @Bean
+    // public ThymeleafViewResolver viewResolver() {
+    //     ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+    //     viewResolver.setTemplateEngine(templateEngine());
+    //     viewResolver.setOrder(1);
+    //     return viewResolver;
+    // }
 
 
 }
